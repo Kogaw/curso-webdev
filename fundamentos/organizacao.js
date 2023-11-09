071ad12186e8cde680e4c4611550ba51be92c633
Ogawa Kei <keisiotane@gmail.com>
2023-11-09 20:01:20 -0300

    update

--- fundamentos/organizacao.js
+++ fundamentos/organizacao.js
@@ -5,6 +5,7 @@ console.log('Sentença de código')
     {
         console.log('Olá')
         console.log('Mundo!')
+        console.log('Eu odeio muito calor')
 
     }   
 }
---
+++ fundamentos/variaveisEconstantes.js
@@ -0,0 +1,16 @@
+var a = 3
+let  
+
+var a = 30
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 4000
+
+console.log(a, b)
+
+const c = 5
+
+console.log(c)
