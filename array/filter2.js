0e2b0cfc12c59a7a35e0a213217285874e00f415
Ogawa Kei <keisiotane@hotmail.com>
2023-12-06 22:01:26 -0300

    update array

--- array/filter2.js
+++ array/filter2.js
@@ -1,3 +1,14 @@
+Array.prototype.filter2 = function(callback){
+    const newArray = []
+    for(let i = 0; i < this.length; i++){
+        if(callback(this[i], i, this)){
+            newArray.push(this[i])
+        }
+    }
+}
+
+
+
 const produtos = [
     {nome: 'Notebook', preco: 2499, fragil: true},
     {nome: 'iPad Pro', preco: 4199, fragil: true},
@@ -10,5 +21,3 @@ const caro = produto => produto.preco >= 500
 const fragil = produto => produto.fragil
  
 console.log(produtos.filter2(caro).filter2(fragil))
- 
-console.log(produtos.filter(caro).filter(fragil))
